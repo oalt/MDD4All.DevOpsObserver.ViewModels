e6f62898b11bb8a0baa5cdef147efba4112a15b2
oalt <oa@lacheweg21.de>
2024-07-12 17:20:14 +0200

    Initial source commit

---
+++ src/MDD4All.DevOpsObserver.ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.DevOpsObserver.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<StatusViewModel> StatusViewModels { get; set; } = new List<StatusViewModel>();
+    }
+}
---
+++ src/MDD4All.DevOpsObserver.ViewModels/StatusViewModel.cs
@@ -0,0 +1,54 @@
+﻿using MDD4All.DevOpsObserver.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.DevOpsObserver.ViewModels
+{
+    public class StatusViewModel
+    {
+
+        private DevOpsStatusInformation _statusInformation;
+
+        public StatusViewModel(DevOpsStatusInformation statusInformation) 
+        {
+            _statusInformation = statusInformation;
+        }
+
+        public string CssClass
+        {
+            get
+            {
+                string result = "card ";
+
+                if(_statusInformation.Status == DevOpsStatus.Unknown)
+                {
+                    result += "statusUnknown";
+                }
+                else if(_statusInformation.Status == DevOpsStatus.Success) 
+                {
+                    result += "statusSuccess";
+                }
+                else if(_statusInformation.Status == DevOpsStatus.Fail)
+                {
+                    result += "statusFail";
+                }
+                else if (_statusInformation.Status == DevOpsStatus.Error)
+                {
+                    result += "statusError";
+                }
+
+                return result;
+            }
+        }
+
+        public DevOpsStatusInformation Status
+        {
+            get
+            {
+                return _statusInformation;
+            }
+        }
+
+    }
+}
