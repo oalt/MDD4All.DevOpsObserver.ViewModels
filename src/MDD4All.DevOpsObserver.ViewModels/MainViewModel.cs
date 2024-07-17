437f04d708e212466c457bfc98b091fbcf7bb96f
oalt <oa@lacheweg21.de>
2024-07-17 17:46:15 +0200

    Implementation of the view model

--- src/MDD4All.DevOpsObserver.ViewModels/MainViewModel.cs
+++ src/MDD4All.DevOpsObserver.ViewModels/MainViewModel.cs
@@ -1,11 +1,109 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MDD4All.DevOpsObserver.DataModels;
+using MDD4All.DevOpsObserver.DataProviders.Integration;
+using MDD4All.DevOpsObserver.StatusCalculation;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Timers;
 
 namespace MDD4All.DevOpsObserver.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableObject
     {
+        private IConfiguration _configuration;
+        private HttpClient _httpClient;
+        private DevOpsConfiguration _devOpsConfiguration;
+
+        private StatusCalculator _statusCalculator = new StatusCalculator();
+
+        private IntegrationStatusProvider _integrationStatusProvider;
+        private Timer _timer;
+
+        public MainViewModel(IConfiguration configuration, HttpClient httpClient,
+                             DevOpsConfiguration devOpsConfiguration)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+            _devOpsConfiguration = devOpsConfiguration;
+
+            _integrationStatusProvider = new IntegrationStatusProvider(configuration, httpClient);
+
+            _timer = new Timer(300000);
+            _timer.Elapsed += OnTimerElapsed;
+
+            Task.Run(() => RefreshStatusDataAsync()).Wait();
+
+            _timer.AutoReset = true;
+            //_timer.Enabled = true;
+        }
+
+        private async Task RefreshStatusDataAsync()
+        {
+            List<DevOpsStatusInformation> resultingStatus = new List<DevOpsStatusInformation>();
+
+            foreach (DevOpsSystem devOpsSystem in _devOpsConfiguration.Systems)
+            {
+                List<DevOpsStatusInformation> status = await _integrationStatusProvider.GetDevOpsStatusListAsync(devOpsSystem);
+                resultingStatus.AddRange(status);
+            }
+
+            StatusViewModels.Clear();
+
+            foreach (DevOpsStatusInformation devOpsStatusInformation in resultingStatus)
+            {
+                StatusViewModel statusViewModel = new StatusViewModel(devOpsStatusInformation);
+                StatusViewModels.Add(statusViewModel);
+            }
+
+            StatusViewModels.Sort();
+
+            _refreshingStatus = false;
+
+            OnPropertyChanged();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            _refreshingStatus = true;
+            Task.Run(() => RefreshStatusDataAsync()).Wait();
+        }
+
         public List<StatusViewModel> StatusViewModels { get; set; } = new List<StatusViewModel>();
+
+        private bool _refreshingStatus = false;
+
+        public bool RefreshingStatus
+        {
+            get
+            {
+                return _refreshingStatus;
+            }
+        }
+
+        public StatusViewModel OverallStatus
+        {
+            get
+            {
+                List<DevOpsStatusInformation> devOpsStatusInformationList = new List<DevOpsStatusInformation>();
+
+                foreach(StatusViewModel statusViewModel in StatusViewModels)
+                {
+                    devOpsStatusInformationList.Add(statusViewModel.Status);
+                }
+
+                DevOpsStatusInformation statusInformation = new DevOpsStatusInformation
+                {
+                    Alias = "Overall Status",
+                    Status = _statusCalculator.CalculateOverallState(devOpsStatusInformationList)
+                };
+
+                StatusViewModel result = new StatusViewModel(statusInformation);
+
+                return result;
+            }
+        }
+
     }
 }
--- src/MDD4All.DevOpsObserver.ViewModels/StatusViewModel.cs
+++ src/MDD4All.DevOpsObserver.ViewModels/StatusViewModel.cs
@@ -1,18 +1,21 @@
 ﻿using MDD4All.DevOpsObserver.DataModels;
+using MDD4All.DevOpsObserver.StatusCalculation;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading;
 
 namespace MDD4All.DevOpsObserver.ViewModels
 {
-    public class StatusViewModel
+    public class StatusViewModel : IComparable<StatusViewModel>
     {
 
         private DevOpsStatusInformation _statusInformation;
 
-        public StatusViewModel(DevOpsStatusInformation statusInformation) 
+        private StatusCalculator _statusCalculator;
+
+        public StatusViewModel(DevOpsStatusInformation statusInformation)
         {
             _statusInformation = statusInformation;
+            _statusCalculator = new StatusCalculator();
         }
 
         public string CssClass
@@ -21,21 +24,46 @@ namespace MDD4All.DevOpsObserver.ViewModels
             {
                 string result = "card ";
 
-                if(_statusInformation.Status == DevOpsStatus.Unknown)
+                DevOpsStatus status = _statusCalculator.CalculateDisplayStatus(_statusInformation);
+
+                if (status == DevOpsStatus.Unknown)
                 {
                     result += "statusUnknown";
                 }
-                else if(_statusInformation.Status == DevOpsStatus.Success) 
+                else if (status == DevOpsStatus.Success)
                 {
                     result += "statusSuccess";
                 }
-                else if(_statusInformation.Status == DevOpsStatus.Fail)
+                else if (status == DevOpsStatus.Fail)
                 {
                     result += "statusFail";
+                }
+                else if (status == DevOpsStatus.Warning)
+                {
+                    result += "statusWarning";
                 }
                 else if (_statusInformation.Status == DevOpsStatus.Error)
                 {
                     result += "statusError";
+                }
+
+                return result;
+            }
+        }
+
+        public string IconCssClass
+        {
+            get
+            {
+                string result = "";
+
+                if(_statusInformation.GitServerType == "Github")
+                {
+                    result = "bi bi-github";
+                }
+                else if(_statusInformation.GitServerType == "Bitbucket")
+                {
+                    result = "bi bi-git";
                 }
 
                 return result;
@@ -50,5 +78,50 @@ namespace MDD4All.DevOpsObserver.ViewModels
             }
         }
 
+        public int CompareTo(StatusViewModel other)
+        {
+            int result = 0;
+
+            if (Status.Status == DevOpsStatus.Unknown)
+            {
+                if (other.Status.Status != DevOpsStatus.Unknown)
+                {
+                    result = 1;
+                }
+            }
+            else if (Status.Status == DevOpsStatus.Success)
+            {
+                if (other.Status.Status == DevOpsStatus.Error || other.Status.Status == DevOpsStatus.Fail)
+                {
+                    result = 1;
+                }
+            }
+            else if (Status.Status == DevOpsStatus.Fail)
+            {
+                if (other.Status.Status == DevOpsStatus.Error)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = 0;
+                }
+            }
+            else if (Status.Status == DevOpsStatus.Error)
+            {
+                if (other.Status.Status == DevOpsStatus.Error)
+                {
+                    result = 0;
+                }
+                else
+                {
+                    result = -1;
+                }
+            }
+
+            return result;
+        }
     }
+
 }
+
