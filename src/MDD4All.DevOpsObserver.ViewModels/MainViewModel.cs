b98bd0699addb7e2311e0a557a11ca071335f389
oalt <oa@lacheweg21.de>
2024-07-26 10:18:40 +0200

    Add release version property and chnaging aggregated status calculation

--- src/MDD4All.DevOpsObserver.ViewModels/MainViewModel.cs
+++ src/MDD4All.DevOpsObserver.ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MDD4All.DevOpsObserver.DataModels;
 using MDD4All.DevOpsObserver.DataProviders.Integration;
 using MDD4All.DevOpsObserver.StatusCalculation;
@@ -10,6 +11,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Windows.Input;
 
 namespace MDD4All.DevOpsObserver.ViewModels
 {
@@ -36,6 +38,8 @@ namespace MDD4All.DevOpsObserver.ViewModels
 
             _integrationStatusProvider = new IntegrationStatusProvider(configuration, httpClient);
 
+            InitializeCommands();
+
             _timer = new Timer();
             _timer.Elapsed += OnTimerElapsed;
             _timer.AutoReset = true;
@@ -49,6 +53,11 @@ namespace MDD4All.DevOpsObserver.ViewModels
             _clockRefreshTimer.Start();
 
             RefreshStatusDataAsync();
+        }
+
+        private void InitializeCommands()
+        {
+            RefreshStatusCommand = new RelayCommand(ExecuteRefreshStatusCommand);
         }
 
         private void OnClockTimerElapsed(object sender, ElapsedEventArgs e)
@@ -105,6 +114,8 @@ namespace MDD4All.DevOpsObserver.ViewModels
 
             LastDataRefresh = DateTime.Now;
 
+            SetOverallStatus();
+
             _refreshingStatus = false;
 
             StatusAvailable = true;
@@ -129,28 +140,33 @@ namespace MDD4All.DevOpsObserver.ViewModels
 
         public bool StatusAvailable { get; set; } = false;
 
-        public DateTime? LastDataRefresh { get; set; } = null; 
+        public DateTime? LastDataRefresh { get; set; } = null;
+
+        private StatusViewModel _overallStatus = new StatusViewModel(new DevOpsStatusInformation());
+
+        private void SetOverallStatus()
+        {
+            List<DevOpsStatusInformation> devOpsStatusInformationList = new List<DevOpsStatusInformation>();
+
+            foreach (StatusViewModel statusViewModel in StatusViewModels)
+            {
+                devOpsStatusInformationList.Add(statusViewModel.Status);
+            }
+
+            DevOpsStatusInformation statusInformation = new DevOpsStatusInformation
+            {
+                Alias = "Overall Status",
+                StatusValue = _statusCalculator.CalculateOverallState(devOpsStatusInformationList)
+            };
+
+            _overallStatus = new StatusViewModel(statusInformation);
+        }
 
         public StatusViewModel OverallStatus
         {
             get
             {
-                List<DevOpsStatusInformation> devOpsStatusInformationList = new List<DevOpsStatusInformation>();
-
-                foreach (StatusViewModel statusViewModel in StatusViewModels)
-                {
-                    devOpsStatusInformationList.Add(statusViewModel.Status);
-                }
-
-                DevOpsStatusInformation statusInformation = new DevOpsStatusInformation
-                {
-                    Alias = "Overall Status",
-                    StatusValue = _statusCalculator.CalculateOverallState(devOpsStatusInformationList)
-                };
-
-                StatusViewModel result = new StatusViewModel(statusInformation);
-
-                return result;
+                return _overallStatus;
             }
         }
 
@@ -278,6 +294,29 @@ namespace MDD4All.DevOpsObserver.ViewModels
             {
                 return _devOpsConfiguration.OrganizationTitle;
             }
+        }
+
+        public string AppReleaseVersion
+        {
+            get
+            {
+                string result = "";
+
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.Diagnostics.FileVersionInfo fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+                string version = fileVersionInfo.FileVersion;
+
+                result = "v" + version;
+
+                return result;
+            }
+        }
+
+        public ICommand RefreshStatusCommand { get; private set; }
+
+        private void ExecuteRefreshStatusCommand()
+        {
+            RefreshStatusDataAsync();
         }
     }
 }
